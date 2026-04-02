8ad1c519b4e56e802096f8f2b392b43d588d733c
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-04-03 03:00:27 +0530

    Register MonacoEditorInterop and SqlJsInterop in DI
    Wires up JS interop services for Monaco editor and sql.js SQLite
    engine in the WebAssembly host builder.

--- src/SchemaForge.Wasm/Program.cs
+++ src/SchemaForge.Wasm/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SchemaForge.Core.DdlGeneration;
 using SchemaForge.Core.Services;
 using SchemaForge.Core.State;
+using SchemaForge.Shared.Interop;
 using SchemaForge.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,9 +14,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<SchemaState>();
 builder.Services.AddSingleton<QueryState>();
 
-// Services
+// DDL Generation
 builder.Services.AddSingleton<IDdlGeneratorService, SqliteDdlGenerator>();
 builder.Services.AddSingleton<DdlGeneratorFactory>(sp =>
     new DdlGeneratorFactory(sp.GetServices<IDdlGeneratorService>()));
+
+// JS Interop Services
+builder.Services.AddSingleton<MonacoEditorInterop>();
+builder.Services.AddSingleton<ISqlExecutionService, SqlJsInterop>();
 
 await builder.Build().RunAsync();
